f72b2e1f4e65b4ec971a9da4b6c2e8eb5d1cb9a3
Student <yhashem3@gatech.edu>
2025-02-13 15:38:55 -0500

    Implement charge shifting (needs testing, probably doesn't work)
    Implement a basic charge shifting algorithm. Creates two lists of
    charges, `original_charges` (based on initial configuration) and
    `shifted_charges` (shifts charges off M1 atoms provided in user input).
    Does the MM calculation just using the original charges, then swaps in
    the `shifted_charges` to the system, does the QM calculation, and
    finally swaps back to `original_charges`.
    
    Hasn't been tested yet. High probability of faulty implementation.

--- pydft_qmmm/plugins/link/link.py
+++ pydft_qmmm/plugins/link/link.py
@@ -49,24 +49,69 @@ class LINK(CompositeCalculatorPlugin):
         """
         self._modifieds.append(type(calculator).__name__)
         self.system = calculator.system
-        # Grab the QM and MM calculators so we can do them separately
+        calculator.calculate = self._modify_calculate(
+            calculator.calculate,
+        )
+        # Grab the QM and MM calculators interfaces so we can do them separately
         for calc in calculator.calculators:
             if isinstance(calc, InterfaceCalculator):
                 if isinstance(calc.interface, QMInterface):
                     self.qm_interface = calc.interface
+                    self.qm_calculator = calc
                 elif isinstance(calc.interface, MMInterface):
                     self.mm_interface = calc.interface
+                    self.mm_calculator = calc
 
-        # Get original charges
-        original_charges = system.charges
-        # Prepare array of "shifted charges" to use later
-        shifted_charges = original_charges
-        for i, b_pair in enumerate(boundary_atoms):
-            q_0 = shifted_charges[b_pair[1][0]]
-            shifted_charges[b_pair[1][0]] = 0 # zero out M1 charge
-            n = len(b_pair[1]) - 1.0 # number connected to M1
-            if n > 0:
-                for j in range(n):
+    def _modify_calculate(
+            self,
+            calculate: CalculateMethod,
+    ) -> CalculateMethod:
+        """Modify the calculate routine to use the link-atom method.
+
+        Args:
+            calculate: The calculation routine to modify.
+
+        Returns:
+            The modified calculation routine which implements
+            the link-atom method using H atoms on the boundary
+        """
+        def inner(
+                return_forces: bool | None = True,
+                return_components: bool | None = True,
+        ) -> Results:
+            # Get original charges
+            original_charges = system.charges
+            # Prepare array of "shifted charges" to use later
+            shifted_charges = original_charges
+            for i, b_pair in enumerate(boundary_atoms):
+                q_0 = shifted_charges[b_pair[1][0]]
+                shifted_charges[b_pair[1][0]] = 0 # zero out M1 charge
+                n = len(b_pair[1]) - 1.0 # number connected to M1
+                if n > 0:
+                    for j in range(n):
                     # redistribute charges
                     shifted_charges[b_pair[1][j+1]] += q_0/n
+
+            # from the stock calculate method
+            energy = 0.
+            forces = np.zeros(system.forces.shape)
+            components: Components = dict() # figure out what this means
+            
+            # do the MM calculation first, with the real charges
+            results = mm_calculator.calculate()
+            energy += results.energy
+            forces += results.forces
+            components[f"{mm_calculator.name}_0"] = results.energy # there is probably a more general way for this
+            components["."*1] = results.components
+
+            system.charges(shifted_charges) # shift the charges
+
+            results = qm_calculator.calculate()
+            energy += results.energy
+            forces += results.forces
+            components[f"{qm_calculator.name}_1"] = results.energy
+            components["."*2] = results.components
+
+            system.charges(original_charges) # re-shift the charges
+            results = Results(energy, forces, components)
 
